9d0889f59668dfb417974d478b4821b9b23267ba
Parisa <134794618+parisa-0@users.noreply.github.com>
2025-06-21 16:51:13 +0100

    Update ResourceController.java

--- gs-rest-service/initial/src/main/java/com/example/restservice/ResourceController.java
+++ gs-rest-service/initial/src/main/java/com/example/restservice/ResourceController.java
@@ -4,6 +4,9 @@ package com.example.restservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.MediaType;
 
 @RestController
 public class ResourceController {
@@ -19,6 +22,13 @@ public class ResourceController {
     {
         return EmployeeManager.getAllEmployees();
     }
+    @PostMapping(
+            path = "/employees",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
 
+    public Employee addEmployee(@RequestBody Employee employee) {
+        return employeeManager.addEmployee(employee);
+    }
 
 }
