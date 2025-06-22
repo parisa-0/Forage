a4c7a7cbc70397c4913e17b18ba2b9e31c32ed77
Parisa <134794618+parisa-0@users.noreply.github.com>
2025-06-22 18:11:24 +0100

    Update ResourceController.java

--- gs-rest-service/initial/src/main/java/com/example/restservice/ResourceController.java
+++ gs-rest-service/initial/src/main/java/com/example/restservice/ResourceController.java
@@ -2,11 +2,14 @@ package com.example.restservice;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.http.MediaType;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 public class ResourceController {
@@ -20,15 +23,21 @@ public class ResourceController {
 
     public Employees getEmployees()
     {
-        return EmployeeManager.getAllEmployees();
+        return employeeManager.getAllEmployees();
     }
-    @PostMapping(
-            path = "/employees",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
 
-    public Employee addEmployee(@RequestBody Employee employee) {
-        return employeeManager.addEmployee(employee);
+    @PostMapping(path = "/", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Object> addEmployee(@RequestBody Employee employee)
+    {
+        employeeManager.addEmployee(employee);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(employee.getEmployee_id())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 
 }
